be46fc7cb81ff3067c2c71a4ac0287f0f1bc8e67
Erick Ochieng <erickochieng766@gmail.com>
2023-02-10 09:01:09 +0300

    Passed Test

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length==1
+    array[0]
+  elsif array.length==2
+    array.join(' and ')
+  elsif array.length==3
+    array[0..1].map { |word| word << (', ') }.join("") << "and #{array[2]}"
+  else
+    array[0..array.length-2].map { |word| word << (', ') }.join("") << "and #{array[-1]}"
+  end
 end
